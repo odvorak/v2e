3356754d51e36403da15a3e8c4e83cf9b2ab1a2f
odvorak <54942164+odvorak@users.noreply.github.com>
2025-03-05 20:55:00 +0100

    change target

--- run_v2e_dsec.py
+++ run_v2e_dsec.py
@@ -62,7 +62,7 @@ root_dir= Path("/root/train_40_videos/")
 
 # Iterate through all subfolders
 for subfolder in root_dir.iterdir():
-    if subfolder.is_dir():
+    if subfolder.is_dir() and subfolder != "D10-40":
         flight_mp4 = subfolder / "flight.mp4"
         events_h5 = subfolder / "events.h5"
 
